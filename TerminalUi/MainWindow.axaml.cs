2c05b6ad87c332a88690f39600a726b90a25f48f
Sean <hses106021@gmail.com>
2026-07-17 09:26:28 +0800

    Add project files.

---
+++ TerminalUi/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TerminalUi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
